5ee18dac0333449cf5e813f638d480155ca1d47f
jason <jiexinzhang@sina.cn>
2019-04-17 19:11:35 +0800

    matrixexchange1
    matrixexchange1，非方阵

---
+++ course_example/basic_syntax/matrixexchange1.py
@@ -0,0 +1,18 @@
+'''
+非方阵的转换
+'''
+
+matrix = [[1,2,3],[4,5,6]]
+
+tm = []
+count = 0
+for row in matrix:
+    for i, col in enumerate(row):
+        if len(tm) < i + 1:
+            tm.append([])
+        tm[i].append(col)
+        count += 1
+
+print(matrix)
+print(tm)
+print(count)
