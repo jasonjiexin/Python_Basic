b43c9abe7bba04dc2eb123011f8522cfd4de27bd
jason <jiexinzhang@sina.cn>
2019-05-27 14:41:59 +0800

    字符串
    字符串复习

--- course_example/string/zifuchuanchazhao2.py
+++ course_example/string/zifuchuanchazhao2.py
@@ -3,13 +3,14 @@ index与rindex 都是在字符串中找元素并返回索引，
 index与rindex 找不到都会报错
 原理：
 现在锁定查找的区间然后再进行检索并返回索引值，类型为int
+index() 从左往右找，rindex() 从右往左找
 '''
 
 s = "i am very very very sorry"
 print(s.index('very'))
 print(s.index('very',5))
 # index查找不到元素的时候报错
-#print(s.index('very',6,13))
+# print(s.index('very',6,13))
 print(s.index('very',10))
 print(s.rindex('very',10))
 print(s.rindex('very',10,15))
--- course_example/string/zifuchuangeshihua2.py
+++ course_example/string/zifuchuangeshihua2.py
@@ -1,26 +1,26 @@
 '''
-python 的format函数
+python 的format函数格式化字符串
 '''
 # {} 代表输出的内容，其中数字定位输出的内容，('zhang', 'xin')整体是元组的第一个元素
-print("{0[0]} {0[1]}".format(('zhang','xin')))
+print("{0[0]} {0[1]}".format(('zhang','xin')))   #{0[0]} 表示输出内容的第一个元素('zhang','xin')中的第一个元素'zhang'
 print("{0[0]} * {0[1]}".format(('zhang','xin')))
 print("{0[1]} * {0[0]}".format(('zhang','xin')))
 
-# {0} 表示将('zhang','xin') 整体输出
+# {0} 表示将输出内容的第一个元素('zhang','xin') 进行输出
 print("{0} * {0}".format(('zhang','xin')))
-
-
 # print("{} * {}".format(('zhang','xin'))) IndexError: tuple index out of range；前面要求展现两个{} {} 元素，后面元素中只有一个元素，应修改为以下方式
 print("{} * {}".format('zhang','xin'))
+
 
 print("{0} * {0}".format(('zhang','xin')[0]))
 print("{} * {}".format(('zhang','xin')[0], ('  ', 'com')[1]))
 
+print("-----"*10)
 # format 传入的是需要展示的数据，
 print('{0}*{1}={2:<2}'.format(3,2,2*3))
 # < 是左对齐，
-print('{0}*{1}={2:<02}'.format(3,2,2*3))
-print('{0}*{1}={2:>02}'.format(3,2,2*3))
+print('{0}*{1}={2:<02}'.format(3,2,2*3))   #{2:<02} < 左对齐，两位有效数字，不够位用0填充
+print('{0}*{1}={2:>02}'.format(3,2,2*3))   #{2:>02} > 右对齐，两位有效数字，不够位用0填充
 print('{0}*{1}={2}'.format(3,2,2*3))
 #默认为右对齐
 print('{0}*{1}={2:02}'.format(3,2,2*3))
