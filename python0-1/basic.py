3e0e9571cfb917e877d063e134aeb128cb382ef8
jason <jiexinzhang@sina.cn>
2019-05-28 18:04:07 +0800

    basic
    basic

---
+++ python0-1/basic.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a
+
+
+# In[4]:
+
+
+a=1
+print(a)
+
+
+# In[5]:
+
+
+one=two=three=10
+
+
+# In[8]:
+
+
+print(id(one),id(two),id(three))
+
+
+# In[16]:
+
+
+var01,var02=2,1
+print(id(var01),id(var02))
+
+
+# In[2]:
+
+
+name='''jason'''
+name1="zhang"
+name2='tom'
+print(name,name1,name2,'<<me is me>>')
+
+
+# In[21]:
+
+
+#三引号换行
+text='''woemsdf 
+sdfas d
+sdfasdfasdfa    asfasd[\t] 
+asdfa sdsd'''
+print(text)
+
+
+# In[33]:
+
+
+text="woemsdf 
+sdfas d
+sdfasdfasdfa    asfasd[\t] 
+asdfa sdsd"
+print(text)
+
+
+# In[39]:
+
+
+name="Tom is a cat"
+print(name)
+print(name[3])
+print(name[4:6])
+print(name[:3])
+print(name[:])
+print(name[-4:])
+
+
+# In[3]:
+
+
+name='tom'
+job='teacher'
+record=name+' '+job
+print(record)
+print(name,job)
+
+
+# In[48]:
+
+
+name='ton is teacher!'
+name1="tom"+ name[3:]
+
+print(len(name1))
+
+
+# In[51]:
+
+
+addr=r"C:\wei\home\name"
+print(addr)
+
+
+# In[52]:
+
+
+print('cat'*20)
+
+
+# In[56]:
+
+
+age=10
+print("tom is %d"%(age))
+
+
+# In[ ]:
+
+
+
+
